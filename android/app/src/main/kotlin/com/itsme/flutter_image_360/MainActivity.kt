d28c224db857534e1acfff57d111690d9e91cce1
Abu Baker Nawaz <abnawaz14@gmail.com>
2021-02-01 23:14:25 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/itsme/flutter_image_360/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.itsme.flutter_image_360
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'cardekho.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DemoPage(
+        title: "Demo",
+      ),
+    );
+  }
+}
